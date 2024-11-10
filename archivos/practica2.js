5c4ca1de70e53d79ec9b1c3accee91e071de7160
eeveenax <eveliagilparedes@gmail.com>
2024-11-11 00:10:22 +0100

    cambios1

--- archivos/practica2.js
+++ archivos/practica2.js
@@ -7,17 +7,6 @@ var navegador = document.getElementById("navegador");
 var fecha = document.querySelector(".fecha");
 var tiempo = document.querySelector(".tiempo");
 
-var proceso1 = document.getElementById("proceso1");
-var proceso2 = document.getElementById("proceso2");
-var proceso3 = document.getElementById("proceso3");
-var imagen1 = document.getElementById("imagenp1");
-var imagen2 = document.getElementById("imagenp2");
-var imagen3 = document.getElementById("imagenp3");
-
-var titulo1 = document.getElementById("proceso1titulo");
-var titulo2 = document.getElementById("proceso2titulo");
-var titulo3 = document.getElementById("proceso3titulo");
-
 menu.addEventListener("click", function () {
   if (navegador.style.display == "block") {
     navegador.style.display = "none";
@@ -62,23 +51,3 @@ function relojDigital() {
 setInterval(() => {
   relojDigital();
 }, 1000);
-
-function visibiliad(imagen, proceso) {
-  imagen.addEventListener("click", function () {
-    if (proceso.style.display === "none") {
-      proceso.style.display = "block"; // Mostrar el elemento proceso1
-      imagen.style.display = "none"; // Ocultar imagen1
-    }
-
-    proceso.addEventListener("click", function () {
-      if (imagen.style.display === "none") {
-        proceso.style.display = "none"; // Mostrar el elemento proceso1
-        imagen.style.display = "block"; // Ocultar imagen1
-      }
-    });
-  });
-}
-
-visibiliad(imagen1, proceso1);
-visibiliad(imagen2, proceso2);
-visibiliad(imagen3, proceso3);
--- archivos/subpaginas/subtitulacion/subtitulacion.js
+++ archivos/subpaginas/subtitulacion/subtitulacion.js
@@ -3,20 +3,6 @@ const navegador = document.getElementById("navegador");
 const disney = document.getElementById("disney");
 const info = document.getElementById("info");
 
-var proceso1 = document.getElementById("paso1");
-var proceso2 = document.getElementById("paso2");
-var proceso3 = document.getElementById("paso3");
-var proceso4 = document.getElementById("paso4");
-var proceso5 = document.getElementById("paso5");
-var proceso6 = document.getElementById("paso6");
-
-var imagen1 = document.getElementById("imagent1");
-var imagen2 = document.getElementById("imagent2");
-var imagen3 = document.getElementById("imagent3");
-var imagen4 = document.getElementById("imagent4");
-var imagen5 = document.getElementById("imagent5");
-var imagen6 = document.getElementById("imagent6");
-
 menu.addEventListener("click", function () {
   navegador.classList.toggle("mostrar");
 });
@@ -25,28 +11,3 @@ disney.addEventListener("click", function () {
   disney.classList.toggle("volteado");
   info.classList.toggle("mostrar");
 });
-
-function visibilidad(imagen, paso) {
-  imagen.addEventListener("click", function () {
-    if (paso.style.display === "none") {
-      paso.style.display = "block";
-      imagen.style.display = "none";
-    }
-  });
-
-  paso.addEventListener("click", function () {
-    if (imagen.style.display === "none") {
-      imagen.style.display = "block";
-      paso.style.display = "none";
-    }
-  });
-}
-
-// Aplica la función a todas las imágenes y pasos
-visibilidad(imagen1, proceso1);
-visibilidad(imagen2, proceso2);
-visibilidad(imagen3, proceso3);
-visibilidad(imagen4, proceso4);
-visibilidad(imagen5, proceso5);
-visibilidad(imagen5, proceso5);
-visibilidad(imagen6, proceso6);
