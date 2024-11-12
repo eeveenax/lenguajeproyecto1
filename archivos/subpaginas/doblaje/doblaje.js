3a6385af47d4804c3d5d62861e0982a1027b267e
eeveenax <eveliagilparedes@gmail.com>
2024-11-12 13:49:31 +0100

    imagenes cambios

--- archivos/subpaginas/doblaje/doblaje.js
+++ archivos/subpaginas/doblaje/doblaje.js
@@ -69,3 +69,18 @@ selectMujeres.addEventListener("change", function () {
     dobladora1.style.display = "none";
   }
 });
+
+$(document).ready(function () {
+  // Aplica VanillaTilt en todos los elementos con la clase '.card'
+  $(".card").each(function () {
+    VanillaTilt.init(this, {
+      maxTilt: 15,
+      perspective: 1400,
+      easing: "cubic-bezier(.03,.98,.52,.99)",
+      speed: 1200,
+      glare: true,
+      maxGlare: 0.2,
+      scale: 1.04,
+    });
+  });
+});
