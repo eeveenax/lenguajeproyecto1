f6f004032bd287125438634e1bb80644fec9129e
eeveenax <eveliagilparedes@gmail.com>
2024-12-18 12:13:06 +0100

    cambios

--- archivos/subpaginas/documentos/docArchivos/documentos.js
+++ archivos/subpaginas/documentos/docArchivos/documentos.js
@@ -4,3 +4,11 @@ const navegador = document.getElementById("navegador");
 menu.addEventListener("click", function () {
   navegador.classList.toggle("mostrar");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var sticky = !!getComputedStyle(document.querySelector("main"))
+    .webkitOverflowScrolling;
+  if (sticky) {
+    document.body.className = "sticky";
+  }
+});
